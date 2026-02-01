b605397c1fe1d97f1be2b5c3f743309d0b26fa0e
Ayush Khatiwada <ayush7khatiwada@gmail.com>
2026-02-01 16:52:41 +0000

    save

--- Section_1_Monte_Carlo_Integrator/src/MonteCarlo.cpp
+++ Section_1_Monte_Carlo_Integrator/src/MonteCarlo.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <random>
 
 bool InsideUnitSphere(double x, double y, double z)
 {
@@ -15,13 +16,15 @@ double IntegrateMonteCarlo3D(int n_points, double min, double max)
     double VolCube = std::pow((max - min), 3);
 
     //set up random number generator here
+    std::uniform_real_distribution<double> uniform_real_dist(-1.0, 1.0);
+    std::mt19937 rng_mt;
 
     for(int i = 0; i < n_points; i++)
     {
         //generate random points here
-        double x = 
-        double y =
-        double z = 
+        double x = uniform_real_dist(rng_mt);
+        double y = uniform_real_dist(rng_mt);
+        double z = uniform_real_dist(rng_mt);
         if(InsideUnitSphere(x, y, z)) count++;
     }
 
